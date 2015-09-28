c6797e918169c40c7965385c07f6b6863cb9bd06
wanghx <whx89768@alibaba-inc.com>
2015-09-28 15:19:38 +0800

    update

--- README.md
+++ README.md
@@ -6,11 +6,8 @@
 ```
 npm install gulp-seajs-concat
 ```
+
 ## Usage:
-```
-var concat = require("gulp-seajs-concat");
-concat(reg) //reg为匹配路径的正则，也就是可以过滤掉自己不想打包的js，参数不必须。
-```
 demo:
 ```     
 var gulp = require("gulp");
@@ -20,8 +17,25 @@ var concat = require("gulp-seajs-concat");
 gulp.task('default' , function(){
    return gulp.src('javascripts/**/*')
        .pipe(transport())
-       .pipe(concat(/\/app\//g))
+       .pipe(concat({
+            alias: {
+                'common': './public/javascript/cmdmodule/base/common',
+                'http': './public/javascript/cmdmodule/base/http'
+            },
+            base: "./pubic/javascript/",
+            filter: /\/app\//g
+        }))
        .pipe(uglify())
        .pipe(gulp.dest("./dist"));
 })
 ```
+
+## API
+### alias
+别名，对应seajs.config的alias，如果在seajs的config里加了alias，最好在concat也配置上该alias，concat才能找到响应文件
+
+### base 
+基础目录，对应seajs.config的base，js文件里require的路径第一个字符不是.的路径将在前面加上base目录。
+
+### filter
+过滤正则，过滤输出文件/目录
--- index.js
+++ index.js
@@ -1,63 +1,107 @@
 var through = require("through2");
 var url = require('url');
+var path = require("path");
 
-module.exports = function (reg) {
-    var denps = {};
-    var _transform = function (file, enc, cb) {
-        if (!file || !file.contents) {
-            return cb();
+module.exports = function(options) {
+  var denps = {};
+
+  var type = Object.prototype.toString.call(options);
+
+  var alias = options.alias || {};
+  var filter = options.filter;
+  var base = options.base || "";
+
+  // 兼容旧版本
+  if(type === "[Object RegExp]"){
+    filter = options;
+  }
+
+  /**
+   * 收集js文件信息
+   */
+  var _transform = function(file, enc, cb) {
+    if (!file || !file.contents) {
+      return cb();
+    }
+
+    var filepath = url.format(file.path);
+    denps[filepath] = file;
+
+    // 复制文件数据
+    file.caculateContents = new Buffer(file.contents.length);
+    file.contents.copy(file.caculateContents);
+    
+    file.children = [];
+
+    var fileString = file.contents.toString();
+    if (/(\[.*?\])/.test(fileString)) {
+      var arr = RegExp.$1.match(/".*?"/g) || [];
+
+      arr.forEach(function(item, i){
+        var jsurl = item.replace(/"/g, '');
+
+        // 如果有别名，则查询别名
+        if(alias){
+          jsurl = alias[jsurl] || jsurl;
         }
 
-        var filepath = url.format(file.path);
-        denps[filepath] = file;
-        denps[filepath].caculateContents = new Buffer(file.contents.length);
-        file.contents.copy(denps[filepath].caculateContents)
+        // 自动补全后缀
+        jsurl = jsurl.indexOf(".js") >= 0 ? jsurl : jsurl + ".js";
 
-        var fileString = file.contents.toString();
-        if (/(\[.*?\])/.test(fileString)) {
-            var arr = RegExp.$1.match(/".*?"/g) || [];
-
-            for (var i = 0; i < arr.length; i++) {
-                arr[i] = arr[i].replace(/"/g, '');
-                var childpath = url.resolve(filepath, arr[i].indexOf(".js") >= 0 ? arr[i] : arr[i] + ".js")
-                denps[filepath].childs = denps[filepath].childs || [];
-                denps[filepath].childs.push(childpath)
-            }
+        // 如果有基本目录，则与基本目录结合
+        if (base && jsurl.charAt(0) !== ".") {
+          jsurl = base + ((base.charAt(base.length) === "/") ? "" : "/") + jsurl;
+          jsurl = path.resolve('', jsurl);
+        } else {
+          jsurl = path.resolve(path.dirname(filepath), jsurl);
         }
 
-        cb();
-    };
+        file.children.push(jsurl);
+      });
 
-    var _flush = function (cb) {
-        var contents = [], pathArr = [], size = 0;
+    }
 
-        for (var filepath in denps) {
-            if (reg && (reg instanceof RegExp) && !filepath.match(reg)) continue;
+    cb();
+  };
 
-            var f = denps[filepath];
-            contents.length = pathArr.length = size = 0;
-            each(f);
-            f.contents = Buffer.concat(contents, size);
-            this.push(f);
-        }
+  /**
+   * 合并文件
+   */
+  var _flush = function(cb) {
+    var contents = [],
+      pathArr = [],
+      file = null,
+      size = 0;
 
-        function each(file) {
-            if (!file)return;
+    for (var filepath in denps) {
+      if (filter && (filter instanceof RegExp) && !filepath.match(filter)) continue;
 
-            if (pathArr.indexOf(file.path) === -1) {
-                pathArr.push(file.path)
-                contents.push(file.caculateContents);
-                size += file.caculateContents.length;
-            }
-            if (file.childs) {
-                file.childs.forEach(function (fc) {
-                    each(denps[fc])
-                });
-            }
-        }
+      contents.length = pathArr.length = size = 0;
 
-        cb();
-    };
+      each(file = denps[filepath]);
 
-    return through.obj(_transform, _flush);
+      file.contents = Buffer.concat(contents, size);
+      this.push(file);
+    }
+
+    function each(file) {
+      if (!file) return;
+
+      if (pathArr.indexOf(file.path) < 0) {
+        pathArr.push(file.path);
+        contents.push(file.caculateContents);
+        size += file.caculateContents.length;
+      }
+
+      if (file.children) {
+        file.children.forEach(function(fc) {
+          each(denps[fc])
+        });
+      }
+    }
+
+    cb();
+  };
+
+  return through.obj(_transform, _flush);
 }
