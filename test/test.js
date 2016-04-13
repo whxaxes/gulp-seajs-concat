40a73515ac90ba8087fc0c1a81220a79f61b2c18
wanghx <whx89768@alibaba-inc.com>
2016-04-13 20:04:29 +0800

    update unit test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var concat = require("../");
 var vfs = require("vinyl-fs");
 var assert = require('assert');
+var expect = require('chai').expect;
 var transport = require("gulp-seajs-transport");
 var fs = require('fs');
 
@@ -15,19 +16,8 @@ describe('test/test.js', function(){
       }))
       .pipe(vfs.dest(__dirname + "/dist/test1"))
       .on('end', function(){
-        var matchera = getMatcher('test1', 'a');
-        var matcherc =  getMatcher('test1', 'c');
-
-        assert(
-          matchera.length === 2,
-          'some file is not concat in a.js, matcher is ' + JSON.stringify(matchera)
-        );
-
-        assert(
-          matcherc.length === 2,
-          'some file is not concat in c.js, matcher is ' + JSON.stringify(matcherc)
-        );
-
+        expect(getMatcher('test1', 'a')).to.have.length(2);
+        expect(getMatcher('test1', 'c')).to.have.length(2);
         done();
       });
   });
@@ -43,19 +33,8 @@ describe('test/test.js', function(){
       }))
       .pipe(vfs.dest(__dirname + "/dist/test2"))
       .on('end', function(){
-        var matchera = getMatcher('test2', 'a');
-        var matcherc =  getMatcher('test2', 'c');
-
-        assert(
-          matchera.length === 3,
-          'some file is not concat in a.js, matcher is ' + JSON.stringify(matchera)
-        );
-
-        assert(
-          matcherc.length === 0,
-          'some file is not concat in c.js, matcher is ' + JSON.stringify(matcherc)
-        );
-
+        expect(getMatcher('test2', 'a')).to.have.length(3);
+        expect(getMatcher('test2', 'c')).to.be.empty;
         done();
       });
   });
